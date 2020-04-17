d2c7f8c9d36d85f2f8ed99380665b8dcb46dfafb
André Medeiros <56321997+admedeiros@users.noreply.github.com>
2020-04-17 10:00:14 -0300

    Tirei#test

--- Scripts/ANOVA/Looping_ANOVA.R
+++ Scripts/ANOVA/Looping_ANOVA.R
@@ -33,4 +33,3 @@ ggboxplot(data, x = "Método", y = "Mean",
   color = "Método", palette = c("#00AFBB", "#53fd10"),
   add = "jitter"
   )+   stat_compare_means(comparisons = my_comparisons, method = "t.test")
-  #Teste
